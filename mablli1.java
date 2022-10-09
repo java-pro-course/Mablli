808fad123012b39e8f5b891adb1af611677ac19a
Лев Шинкоренко <Lev Shinkorenko>
2022-10-09 12:08:15 +0500

    Add mablli1.java

--- mablli1.java
+++ mablli1.java
@@ -4,9 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-     int a = in.nextInt();
+         int sum1 = 0;
+         int sum2 = 0;
 
-        System.out.println(a * 5);
+    for (int i = 0; i <= 100; i += 2)
+        sum1 += i;
+
+    for (int i = 1; i <= 100; i += 2)
+        sum2 += i;
+
+        System.out.println(sum1);
+        System.out.println(sum2);
     }
-    public static Scanner in = new Scanner(System.in);
+       public static Scanner in = new Scanner(System.in);
 }
