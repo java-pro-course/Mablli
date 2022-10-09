32c86070deba535f0260af9e65f4f22739911931
Лев Шинкоренко <Lev Shinkorenko>
2022-10-09 11:32:21 +0500

    Add mablli1.java

---
+++ mablli1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     int a = in.nextInt();
+
+        System.out.println(a * 5);
+    }
+    public static Scanner in = new Scanner(System.in);
+}
